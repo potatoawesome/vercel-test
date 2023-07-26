7e9360928b909f71a7987f89a6e60bb24442ea31
Harshad <xxx>
2023-07-26 10:41:50 +0200

    show localised content

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import { useTranslation } from "../i18n";
+
+export default async function Home({
+  params: { lng },
+}: {
+  params: { lng: string };
+}) {
+  const { t } = await useTranslation(lng);
   return (
     <main>
-      <h1>Hi there!</h1>
+      <h1>{t("title")}</h1>
+      <h2>{t("subtitle")}</h2>
     </main>
   );
 }
